4e360e2331882355ebd3ea0b9833d338e6533256
iniuzan <mfauzanabdurrach84@gmail.com>
2022-02-25 00:08:55 +0700

    Menambahkan file latihan 1.php

---
+++ kuliah/pertemuan3/latihan1.php
@@ -0,0 +1,42 @@
+<?php
+// Pengulangan
+//for
+//while
+//do while
+//foreach : pengulangan khusus array
+
+// for loop
+
+echo "Perulangan For loop <br>";
+for ($i = 0; $i <5; $i++){
+    echo "Hello World!<br>";
+}
+
+echo "<hr>";
+// end for loop
+
+// While loop
+echo "Perulangan While loop <br>";
+$j = 0;
+
+while ($j < 5){
+    echo "Hello Dunia!<br>";
+    
+    $j++;
+    
+}
+echo "<hr>";
+// end while loop
+
+//do while loop
+$k = 0;
+do {
+    echo  "Hello World!<br>";
+    $k++;
+} while ($k < 5);
+// end do while loop
+
+//untuk tabel ada pada file latihan1.1.php
+
+
+?>

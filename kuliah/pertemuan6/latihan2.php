05d1adacd05162a6147353c39ab1b888b6bf732d
iniuzan <mfauzanabdurrach84@gmail.com>
2022-03-27 10:05:24 +0700

    test extension vscode (Format HTML to php) as formatter

--- kuliah/pertemuan6/latihan2.php
+++ kuliah/pertemuan6/latihan2.php
@@ -35,25 +35,28 @@ $mahasiswa = [
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Daftar Mahasiswa</title>
 </head>
+
 <body>
     <h1>Daftar Mahasiswa</h1>
     <?php foreach ( $mahasiswa as $mhs ):?>
-        <ul>
-            <li>
-                <img src="./img/<?=$mhs["gambar"]; ?>" alt="">
-            </li>
-            <li>Nama: <?php echo $mhs["nama"] ?></li>
-            <li>NPM: <?php echo $mhs["npm"] ?></li>
-            <li>Email: <?php echo $mhs["email"] ?></li>
-            <li>Jurusan: <?php echo $mhs["jurusan"] ?></li>
-        </ul>
-        <!-- urutan pada array tidak akan dipedulikan. PHP hanya peduli akan Key-nya. -->
+    <ul>
+        <li>
+            <img src="./img/<?=$mhs["gambar"]; ?>" alt="">
+        </li>
+        <li>Nama: <?php echo $mhs["nama"] ?></li>
+        <li>NPM: <?php echo $mhs["npm"] ?></li>
+        <li>Email: <?php echo $mhs["email"] ?></li>
+        <li>Jurusan: <?php echo $mhs["jurusan"] ?></li>
+    </ul>
+    <!-- urutan pada array tidak akan dipedulikan. PHP hanya peduli akan Key-nya. -->
     <?php endforeach; ?>
 </body>
+
 </html>
